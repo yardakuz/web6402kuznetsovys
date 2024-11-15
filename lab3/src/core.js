4023898a845bd294eb06a601beac0544a7140488
yardakuz <yardakuz@gmail.com>
2024-11-15 18:31:11 +0400

    lab 3

---
+++ lab3/src/core.js
@@ -0,0 +1,149 @@
+/**
+ * Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
+ * @param {*} n
+ */
+function isInteger(n) {
+    return (n | 0) === n;
+}
+
+/**
+ * Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
+ */
+function even() {
+    return Array.from({ length: 10 }, (_, i) => 2 + i * 2);
+}
+
+/**
+ * Напишите функцию, считающую сумму чисел до заданного используя цикл
+ * @param {*} n
+ */
+function sumTo(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+/**
+ * Напишите функцию, считающую сумму чисел до заданного используя рекурсию
+ * @param {*} n
+ */
+function recSumTo(n) {
+    if (n === 1) return 1;
+    return n + recSumTo(n - 1);
+}
+
+/**
+ * Напишите функцию, считающую факториал заданного числа
+ * @param {*} n
+ */
+function factorial(n) {
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
+}
+
+/**
+ * Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
+ * @param {*} n
+ */
+function isBinary(n) {
+    return n > 0 && (n & (n - 1)) === 0;
+}
+
+/**
+ * Напишите функцию, которая находит N-е число Фибоначчи
+ * @param {*} n
+ */
+function fibonacci(n) {
+    if (n <= 1) return n;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        [a, b] = [b, a + b];
+    }
+    return b;
+}
+
+/**
+ * Напишите функцию, которая принимает начальное значение и функцию операции
+ * и возвращает функцию - выполняющую эту операцию.
+ * Если функция операции (operatorFn) не задана - по умолчанию всегда
+ * возвращается начальное значение (initialValue)
+ * @param initialValue
+ * @param operatorFn - (storedValue, newValue) => {operation}
+ */
+function getOperationFn(initialValue, operatorFn) {
+    let storedValue = initialValue;
+    return function (newValue) {
+        if (!operatorFn) return storedValue;
+        storedValue = operatorFn(storedValue, newValue);
+        return storedValue;
+    };
+}
+
+/**
+ * Напишите функцию создания генератора арифметической последовательности.
+ * @param {number} start - число с которого начинается последовательность
+ * @param {number} step  - число шаг последовательности
+ */
+function sequence(start= 0 , step= 1 ) {
+    let current = start;
+    return function () {
+        const result = current;
+        current += step;
+        return result;
+    };
+}
+
+/**
+ * Напишите функцию deepEqual, которая принимает два значения
+ * и возвращает true только в том случае, если они имеют одинаковое значение
+ * или являются объектами с одинаковыми свойствами,
+ * значения которых также равны при сравнении с рекурсивным вызовом deepEqual.
+ */
+function deepEqual(firstObject, secondObject) {
+
+    if (firstObject === secondObject) return true;
+
+    if (typeof firstObject === "number" && typeof secondObject === "number") {
+        if (isNaN(firstObject) && isNaN(secondObject)) {
+            return true;
+        }
+    }
+
+    if (
+        typeof firstObject !== "object" ||
+        firstObject === null ||
+        typeof secondObject !== "object" ||
+        secondObject === null
+    ) {
+        return false;
+    }
+
+    const keysA = Object.keys(firstObject);
+    const keysB = Object.keys(secondObject);
+
+    if (keysA.length !== keysB.length) return false;
+
+    for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(firstObject[key], secondObject[key])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+module.exports = {
+    isInteger,
+    even,
+    sumTo,
+    recSumTo,
+    factorial,
+    isBinary,
+    fibonacci,
+    getOperationFn,
+    sequence,
+    deepEqual,
+};
